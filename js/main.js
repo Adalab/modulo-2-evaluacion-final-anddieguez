47c140c669db864fec3131b5172642c381510b92
anddieguez <andreadiegar@gmail.com>
2023-09-29 20:22:34 +0200

    added img

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@
 const inputSearch = document.querySelector(".js-search-text");
 const btnInput = document.querySelector(".js-search-btn");
 const ulFilms = document.querySelector(".js-films");
-const ulFilmImg = document.querySelector(".js-film-img");
 
-//let dataApi = [];
+
+let dataApi = [];
 
 function handleclick(event) {
   event.preventDefault();
@@ -13,13 +13,13 @@ function handleclick(event) {
   fetch(`https://api.tvmaze.com/search/shows?q=${searchValue}`) //con fetch nos traemos de la api la información que se introduce en el cuadro de búsqueda.
     .then((response) => response.json()) // en el primer then convertimos la primera respuesta al formato json
     .then((data) => {
+        //ulFilms.innerHTML = "";
+        //ulFilmImg.innerHTML = "";
         console.log(data);
-       for (let i = 0; i < data.length; i++) {
-            ulFilms.innerHTML = `<li>${data[i]["show"]["name"]}</li>`;
-            ulFilmImg.src = `${data[i]["show"]["image"]["medium"]}`;
-            //console.log(data[i]["show"]["name"]);  
+        dataApi = data;
+        for (const data of dataApi) {
+            ulFilms.innerHTML += `<li id=${data.show.id}><h2>${data.show.name}</h2><img src=${data.show.image.medium} alt""></li>`;
         }
-      
     });
 }
 
